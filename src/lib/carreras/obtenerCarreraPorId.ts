ca17535bf8bfb31794511aa09d0913c48f22ad31
Anthony Távara <atavara@fi.uba.ar>
2026-08-27 16:11:07 -0300

    fix: cambiar tipo de parametros

--- src/lib/carreras/obtenerCarreraPorId.ts
+++ src/lib/carreras/obtenerCarreraPorId.ts
@@ -1,16 +1,21 @@
 import { carreras } from "../../data/carreras/carreras-index";
 import type { Carrera } from "../../data/carreras/types";
 
-export function obtenerCarreraPorId(id : string) : Carrera{
-    return carreras.find((carrera) => carrera.id == id)
+export function obtenerCarreraPorId(id: string): Carrera | null {
+  const carrera = carreras.find((carrera) => carrera.id == id);
+
+  if (!carrera) return null;
+
+  return carrera;
 }
 
-export function calcularPorcentajeCarerraPorId(id : string, aprobadas : Set<string>) : number{
-    const carrera = obtenerCarreraPorId(id);
+export function calcularPorcentajeCarerraPorId(
+  id: string,
+  aprobadas: Set<string>,
+): number | null {
+  const carrera = obtenerCarreraPorId(id);
 
-    if(!carrera)
-        return null
+  if (!carrera) return null;
 
-    return Math.round(aprobadas.size / carrera.materias.length * 100)
+  return Math.round((aprobadas.size / carrera.materias.length) * 100);
 }
-
