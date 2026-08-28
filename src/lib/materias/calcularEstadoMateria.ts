77aa7dca5a593fcb902ef667618d8ec072558d6e
Anthony Távara <atavara@fi.uba.ar>
2026-08-28 12:36:52 -0300

    feat: obtener carrera

--- src/lib/materias/calcularEstadoMateria.ts
+++ src/lib/materias/calcularEstadoMateria.ts
@@ -1,4 +1,4 @@
-import type { Materia } from "../../data/carreras/types";
+import type { Carrera, Materia } from "../../data/carreras/types";
 
 export type EstadoMateria = "bloqueada" | "disponible" | "aprobada";
 
@@ -14,4 +14,16 @@ export function calcularEstadoMateria(
     materia.correlativas.every((id) => aprobadas.has(id));
 
   return habilitada ? "disponible" : "bloqueada";
+}
+
+export function obtenerMateriaPorCarreraYId(
+  carrera: Carrera,
+  id: string,
+): Materia | null {
+  const materias = carrera.materias;
+  const materia = materias.find((carrera) => carrera.id == id);
+
+  if (!materia) return null;
+
+  return materia;
 }
